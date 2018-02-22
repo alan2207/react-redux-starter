6cd42fb8a6c7e4e6a34a923eccce530d7ee64aae
alan2207 <alan2207@live.com>
2018-02-22 21:06:15 +0100

    updated

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  "env": {
+      "browser": true,
+      "node" : true,
+      "es6": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+      'ecmaVersion': 2017,
+      "ecmaFeatures": {
+          "experimentalObjectRestSpread": true,
+          "jsx": true
+      },
+      "sourceType": "module"
+  },
+  "plugins": [
+      "react"
+  ],
+  "rules": {
+      "indent": [
+          "error",
+          2
+      ],
+      "linebreak-style": [
+          "error",
+          "unix"
+      ],
+      "quotes": [
+          "error",
+          "single"
+      ],
+      "semi": [
+          "error",
+          "always"
+      ],
+      "react/jsx-uses-vars": 1,
+      "react/jsx-uses-react": 1,
+      "no-console": 1,
+      "linebreak-style": 0,
+  }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,15 @@ var config = {
   },
   module: {
     rules: [
-      { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
     ]
   },
   devServer: {
